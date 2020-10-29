2ad31db45547a57a6b885c2ca930a2635eb2aa8c
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-10-28 22:38:50 -0300

    add BrotherNumberService class, add console behavior on main

---
+++ Exercicio1_AlgoritmoBasico/BrotherNumberService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1_AlgoritmoBasico
+{
+    public static class BrotherNumberService
+    {
+        public static int LargestNumberOnNFamily(int number)
+        {
+
+
+            return number;
+        }
+    }
+}
--- Exercicio1_AlgoritmoBasico/Program.cs
+++ Exercicio1_AlgoritmoBasico/Program.cs
@@ -10,6 +10,31 @@ namespace Exercicio1_AlgoritmoBasico
     {
         static void Main(string[] args)
         {
+            bool endApplication = false;            
+
+            while (!endApplication)
+            {
+                Console.WriteLine("Insira um numero inteiro.");
+
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out int value);
+
+                if (parseSuccess)
+                {
+                    int result = BrotherNumberService.LargestNumberOnNFamily(value);
+                    Console.WriteLine($"O maior numero na familia N do numero {value} é {result}.");
+                }
+                else
+                {
+                    Console.WriteLine("Numero inteiro iválido.");
+                }
+
+                Console.WriteLine("Aperte qualquer tecla para recomeçar, ESC para encerrar a aplicação.");
+
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    return;
+            }
         }
     }
 }
