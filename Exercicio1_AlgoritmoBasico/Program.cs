4dd498ac305632e27750bdab96af4a41095d1ea0
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-10-29 21:00:14 -0300

    change result message, add \n on invalid integer message

--- Exercicio1_AlgoritmoBasico/Program.cs
+++ Exercicio1_AlgoritmoBasico/Program.cs
@@ -21,11 +21,11 @@ namespace Exercicio1_AlgoritmoBasico
                 if (parseSuccess)
                 {
                     int result = BrotherNumberService.LargestNumberOnNFamily(value);
-                    Console.WriteLine($"O maior numero na familia N do numero {value} é {result}.");
+                    Console.WriteLine(result);
                 }
                 else
                 {
-                    Console.WriteLine("Numero inteiro iválido.");
+                    Console.WriteLine("\nNumero inteiro inválido.");
                 }
 
                 Console.WriteLine("Aperte qualquer tecla para recomeçar, ESC para encerrar a aplicação.");
